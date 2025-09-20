593b6eafae52fc4c5bb134b2e2ad3567238f9ffd
MH-liton-360 <liton72524nk@gmail.com>
2025-09-21 02:47:06 +0600

    Routing setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,6 +24,7 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+
     </html>
   );
 }
---
+++ src/app/login/nested-signin/page.jsx
@@ -0,0 +1,10 @@
+
+const signin = () => {
+    return (
+        <div>
+            <h1>Nested-page</h1>
+        </div>
+    );
+};
+
+export default signin;
