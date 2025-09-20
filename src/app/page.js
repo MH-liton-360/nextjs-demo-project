1164eef5cdc00aa6ebe6428e280df29300472ae7
MH-liton-360 <liton72524nk@gmail.com>
2025-09-20 20:45:22 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,14 @@
+
+export default function Home() {
+  return (
+
+    <main>
+      <div>
+        <h1>Hello Next.js</h1>
+        <h1>.Next is Built production file, super perfomence and all file merge mini file.</h1>
+        <h1>public folder holo jono kisu easily access korar jonno ba jkno kisu publicly show korar jonno jkno website e show korte caile amra jekno kisu ex: image, logo etc</h1>
+        <h1>layout.js----- index.html replacement</h1>
+      </div>
+    </main>
+  );
+}
